768193307a51b568dc982a86d4380908bc77fd19
jlee <wisdomjh100@naver.com>
2026-04-20 23:29:55 +0900

    chore: 리다이렉션 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-const hostname = window.location.hostname;
-
-if (hostname === "https://www.dgmatst.net") {
-  window.location.replace(
-    `https://dgmatst.net`
-  );
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
